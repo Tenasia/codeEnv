5b0286c8f03f46aca84043e9756aa2a9179db45d
John Alvic Viojan <83643362+Tenasia@users.noreply.github.com>
2022-12-12 20:56:34 +0800

    Add files via upload

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ import mysql.connector
 from tkinter import messagebox, PhotoImage
 from tkinter.ttk import *
 import turtle
+import random
 
 class DataBaseGUI:
     
@@ -16,7 +17,7 @@ class DataBaseGUI:
 
         # VARIABLES
         self.login_frame, self.login_button, self.username, self.password = self.create_login_frame()
-        
+
     def create_login_frame(self):
         
         # INITIALIZE VARIABLES
@@ -58,139 +59,182 @@ class DataBaseGUI:
         return login_frame, login_button, username_info, password_info
 
     def login(self, event=None):
-        
+
         username = self.username.get()
         password = self.password.get()
-              
-        self.login_to_db(username, password)
 
+
+        self.login_to_db(username, password)
     def login_to_db(self, username, password):
         
+        self.username = username
+        self.password = password
+
         if password:
             db = mysql.connector.connect(host='localhost', user=username, password=password, db='college')
             cursor = db.cursor()
         else:
 
-            messagebox.showinfo('Title', 'Input Invalid')
+            messagebox.showinfo('Invalid', 'Input Invalid')
 
             db = mysql.connector.connect(host='localhost', user=username, db='college')
             cursor = db.cursor()
 
-        save_query = "SELECT * FROM STUDENTS"
-
-        try: 
-            cursor.execute(save_query)
-            my_result = cursor.fetchall()
-
-            for i in my_result:
-                print(i)
-            
+        try:  
             self.login_frame.destroy()
-
-            self.create_database_frame()
-            
-            print("Query Executed Successfully")
+            self.serial_id, self.first_name, self.last_name, self.remarks, self.location= self.create_database_frame() 
+            print("Successfully Logged In")
         except:
             print("Error occured")
             db.rollback()
             
+        # finally:
+        #     if db.is_connected():
+        #         cursor.close()
+        #         db.close()
+        #         print("MySQL connection is closed.")
+    
+    def submit(self, event=None):
         
-        finally:
-            if db.is_connected():
-                cursor.close()
-                db.close()
-                print("MySQL connection is closed.")
+        serial_id = self.serial_id.get()
+        surname = self.last_name.get()
+        first_name = self.first_name.get()
+        remarks = self.remarks.get()
+        location = self.location.get()
+
+        db = mysql.connector.connect(host='localhost', user=self.username, password=self.password, db='college')
+        cursor = db.cursor()    
+        try:
+            query = 'INSERT INTO `tb_carem` (`SERIAL_ID`, `SURNAME`, `FIRST_NAME`,`REMARKS`, `LOC`) VALUES (%s, %s, %s, %s, %s);'
+            values = (serial_id, surname, first_name, remarks, location)
+            cursor.execute(query, values)
+            db.commit()
+            
+            messagebox.showinfo('Successful', 'Inserted Data Successfully.')
+            self.serial_id.delete(0, tk.END)
+            self.last_name.delete(0, tk.END)
+            self.first_name.delete(0, tk.END)
+            self.remarks.delete(0, tk.END)
+            self.location.delete(0, tk.END)
+
+            self.serial_id.focus_set()
+        except Exception as e:
+            print(e)
+            db.rollback()
+            db.close()
+
+
+
+        # Deleting the forms after submitting
+        
+
 
     def create_database_frame(self):
         self.window.title("Database Table")
         self.window.geometry(f'{600}x{400}')
+        self.window.bind('<Return>', self.submit)
 
         database_frame = tk.Frame(self.window)
         database_frame.grid(row=0, column=0, padx=10, pady=10)
         
         # LABELS
-        student_id_label = tk.Label(database_frame, text='Student ID')
-        student_id_label.grid(row=0, column=0, pady=10, padx=10)
-        student_id_label.config(font=('Helvetica Bold', 10))
+        serial_id_label = tk.Label(database_frame, text='Serial ID')
+        serial_id_label.grid(row=0, column=0, pady=10, padx=10)
+        serial_id_label.config(font=('Helvetica Bold', 10))
 
         first_name_label = tk.Label(database_frame, text='First Name')
         first_name_label.grid(row=1, column=0, pady=10, padx=10)
         first_name_label.config(font=('Helvetica Bold', 10))
 
-        last_name_label = tk.Label(database_frame, text='Last Name')
-        last_name_label.grid(row=2, column=0, pady=10, padx=10)
-        last_name_label.config(font=('Helvetica Bold', 10))
+        surname_label = tk.Label(database_frame, text='Last Name')
+        surname_label.grid(row=2, column=0, pady=10, padx=10)
+        surname_label.config(font=('Helvetica Bold', 10))
         
-        course_label = tk.Label(database_frame, text='Course')
-        course_label.grid(row=3, column=0, pady=10, padx=10)
-        course_label.config(font=('Helvetica Bold', 10))
+        remarks_label = tk.Label(database_frame, text='Remarks')
+        remarks_label.grid(row=3, column=0, pady=10, padx=10)
+        remarks_label.config(font=('Helvetica Bold', 10))
 
-        
+        location_label = tk.Label(database_frame, text='Location')
+        location_label.grid(row=4, column=0, pady=10, padx=10)
+        location_label.config(font=('Helvetica Bold', 10))
 
         # FORMS
-        student_id = tk.Entry(database_frame, width=30)
-        student_id.grid(row=0, column=1,)
+
+        serial_id = tk.Entry(database_frame, width=30)
+        serial_id.grid(row=0, column=1)
 
         first_name = tk.Entry(database_frame, width=30)
         first_name.grid(row=1, column=1)
 
-        last_name = tk.Entry(database_frame, width=30)
-        last_name.grid(row=2, column=1)
+        surname = tk.Entry(database_frame, width=30)
+        surname.grid(row=2, column=1)
 
-        course = tk.Entry(database_frame, width=30)
-        course.grid(row=3, column=1)
-
+        remarks = tk.Entry(database_frame, width=30)
+        remarks.grid(row=3, column=1)
         
+        location = tk.Entry(database_frame, width=30)
+        location.grid(row=4, column=1)
+        # BUTTONS
         submit_button = tk.Button(database_frame, text='ADD')
-        submit_button.grid(row=0, column=2, padx=25, pady=10, ipadx=35)
-
-        edit_button = tk.Button(database_frame, text='DELETE')
-        edit_button.grid(row=0, column=3, padx=25, pady=10, ipadx=35)
-        # submit_button['command'] = self.submit
-        # submit_button.config(font=('Helvetica Bold', 16))
-
-
-    
-        # edit_button = tk.Button(database_frame, text='EDIT')
-        # edit_button.grid(row=7, column=0, padx=10, pady=10, ipadx=50)
-        # edit_button['command'] = self.submit
-        # edit_button.config(font=('Helvetica Bold', 16))
-
-        # delete_button = tk.Button(database_frame, text='DELETE')
-        # delete_button.grid(row=7, column=1, padx=10, pady=10, ipadx=50)
-        # delete_button['command'] = self.submit
-        # delete_button.config(font=('Helvetica Bold', 16))
-        # spacer = tk.Label(database_frame, text='')
-        # spacer.grid(row=0, column = 0,columnspan=2)
-
+        submit_button.grid(row=0, column=2, padx=25, pady=10, ipadx=25)
+        submit_button['command'] = self.submit
         
+        graph_button = tk.Button(database_frame, text='SHOW GRAPH')
+        graph_button.grid(row=0, column=3, padx=25, pady=10, ipadx=25)
+        graph_button['command'] = self.graphPoints
 
-        # username_label = tk.Button(database_frame, text="REMOVE")
-        # username_label.config(font=('Helvetica Bold', 16))
-        # username_label.grid(row=1, column=3, padx=(40, 0), pady=50)
-    
-    def drawATriangle(self):
+        return serial_id, first_name, surname, remarks, location
+    def graphPoints(self):
 
         # SETUP
         turtle.title('PUP MAIN MAP')
+
         # img = PhotoImage(file='pupicon.png')
         # turtle._Screen._root.iconphoto(True, img)
+
         wn = turtle.Screen()
         t = turtle.Turtle()
 
-        t.shape('circle')
+        # t.shape('circle')
         t.shapesize(0.5, 0.5)
-        wn.bgpic('pup.png')
-        wn.update
+
         
-        locations = [(266, 176), (172, 200), (156, 210), (280, 120), (124, 180)]
-        coordinate = t.xcor(), t.ycor()
-        for i in locations:
+        
+        wn.bgpic('pup.png')
+        wn.update()
+        
+        wn.setworldcoordinates(-250, -250, 250, 250)
+        
+    
+
+        random_locations = []
+        for i in range(10):
+            random_x = random.randrange(-250, 250)
+            random_y = random.randrange(-250, 250)
+
+            random_locations.append((random_x, random_y))
+
+
+        # DRAWING
+
+        t.left(90)
+
+        for i in random_locations:
+            
             t.penup()
             t.goto(i)
+            t.write(i, align='center')
             t.stamp()
-            t.penup()
-    
+            t.pendown()
+
+            if i[0] >= 0 and i[1] >= 0:
+                print('First Quadrant')
+            elif i[0] <= 0 and i[1] >= 0:
+                print('Second Quadrant')
+            elif i[0] <= 0 and i[1] <= 0:
+                print('Third Quadrant')
+            elif i[0] >= 0 and i[1] <= 0:
+                print('Fourth Quadrant')
 
         wn.exitonclick()
 
@@ -221,3 +265,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
